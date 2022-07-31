96ba74ff0adfe48dde635529a09b062790828681
erictooth <1023110+erictooth@users.noreply.github.com>
2022-07-31 18:25:59 +0000

    chore: add a couple of tests

--- src/__tests__/index.test.ts
+++ src/__tests__/index.test.ts
@@ -1,3 +1,46 @@
-it("is empty for now", () => {
-    return;
+import { SmartResource } from "../index";
+
+const sampleResult = "sampledata";
+
+const getSampleResource = () => new SmartResource(async () => sampleResult);
+
+it("provides `null` as the initial value before a fetch occurs", () => {
+    const SampleResource = getSampleResource();
+    const fn1 = jest.fn();
+
+    SampleResource.subscribe(fn1);
+
+    expect(fn1.mock.calls.length).toBe(1);
+    expect(fn1.mock.calls[0][0]).toBe(null);
+});
+
+it("it notifies all subscribers when a new value is available", async () => {
+    const SampleResource = getSampleResource();
+    const fn1 = jest.fn();
+    const fn2 = jest.fn();
+
+    SampleResource.subscribe(fn1);
+    SampleResource.subscribe(fn2);
+
+    SampleResource.fetch();
+
+    await new Promise(process.nextTick);
+
+    expect(fn1.mock.calls.length).toBe(2);
+    expect(fn2.mock.calls.length).toBe(2);
+    expect(fn1.mock.calls[1][0]).toBe(sampleResult);
+});
+
+it("immediately provides a new subscriber with the current value if it's already set", async () => {
+    const SampleResource = getSampleResource();
+
+    SampleResource.fetch();
+
+    await new Promise(process.nextTick);
+
+    const fn1 = jest.fn();
+    SampleResource.subscribe(fn1);
+
+    expect(fn1.mock.calls.length).toBe(1);
+    expect(fn1.mock.calls[0][0]).toBe(sampleResult);
 });
--- src/__tests__/react.test.ts
+++ src/__tests__/react.test.ts
@@ -1,3 +1,22 @@
-it("is empty for now", () => {
-    return;
+import { renderHook } from "@testing-library/react-hooks";
+import { SmartResource } from "../index";
+import { useResourceSnapshot } from "../react";
+
+const sampleResult = "sampledata";
+
+const getSampleResource = () => new SmartResource(async () => sampleResult);
+
+it("does not fetch until SmartResource.fetch() is called", async () => {
+    const SampleResource = getSampleResource();
+    const { result, waitForNextUpdate } = renderHook(() =>
+        useResourceSnapshot(SampleResource)
+    );
+
+    expect(result.current[0]).toBe(null);
+
+    SampleResource.fetch();
+
+    await waitForNextUpdate();
+
+    expect(result.current[0]).toBe(sampleResult);
 });
